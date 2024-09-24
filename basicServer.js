c41776d6cc49a062a6fc521ad32c518f2bd7682c
Vili <veli0119@stud.kea.dk>
2024-09-24 22:31:28 +0200

    express server

--- basicServer.js
+++ basicServer.js
@@ -12,94 +12,94 @@ const messageCollection = "pizzaMenu"; // DRY - Don't Repeat Yourself
 
 //Set up connection to MongoDB
 // Replace the placeholder with your Atlas connection string
-const uri = "mongodb://localhost:27017"; 
+const uri = "mongodb://localhost:27017";
 // default mongodb 27017 port number
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
 });
 
 const server = createServer((req, res) => {
-  // console.log(req);
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow all origins
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Allow specific HTTP methods
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type"); // Allow specific headers
+    // console.log(req);
+    res.setHeader("Access-Control-Allow-Origin", "*"); // Allow all origins
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Allow specific HTTP methods
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type"); // Allow specific headers
 
-  // Connect to MongoDB
-  if (req.method === "POST") {
-    let body = "";
+    // Connect to MongoDB
+    if (req.method === "POST") {
+        let body = "";
 
-    req.on("data", (chunk) => {
-      body += chunk.toString();
-    });
+        req.on("data", (chunk) => {
+            body += chunk.toString();
+        });
 
-    req.on("end", () => {
-      console.log("body", body);
-      const data = JSON.parse(body);
-      console.log("saving data", data);
+        req.on("end", () => {
+            console.log("body", body);
+            const data = JSON.parse(body);
+            console.log("saving data", data);
 
-      createMessage(data).then((result) => {
-        console.log("result", result);
-        const createdMessage = data;
-        createdMessage._id = result.insertedId;
+            createMessage(data).then((result) => {
+                console.log("result", result);
+                const createdMessage = data;
+                createdMessage._id = result.insertedId;
 
-        res.setHeader("Content-Type", "application/json");
-        res.statusCode = 201;
-        res.end(JSON.stringify(createdMessage));
-        return;
-      });
-    });
-  } else if (req.method === "GET") {
-    getMessages().then((messages) => {
-      res.setHeader("Content-Type", "application/json");
-      res.statusCode = 200;
-      res.end(JSON.stringify(messages));
-      return;
-    });
+                res.setHeader("Content-Type", "application/json");
+                res.statusCode = 201;
+                res.end(JSON.stringify(createdMessage));
+                return;
+            });
+        });
+    } else if (req.method === "GET") {
+        getMessages().then((messages) => {
+            res.setHeader("Content-Type", "application/json");
+            res.statusCode = 200;
+            res.end(JSON.stringify(messages));
+            return;
+        });
 
-    // res.statusCode = 200;
-    // res.setHeader('Content-Type', 'application/json');
-    // res.end(JSON.stringify({ a: 1 }));
-  }
+        // res.statusCode = 200;
+        // res.setHeader('Content-Type', 'application/json');
+        // res.end(JSON.stringify({ a: 1 }));
+    }
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 async function getMessages() {
-  try {
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    const messages = await client.db(dbName).collection(messageCollection).find().toArray();
-    console.log("result object", messages);
+    try {
+        // Connect the client to the server (optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        const messages = await client.db(dbName).collection(messageCollection).find().toArray();
+        console.log("result object", messages);
 
-    return messages;
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+        return messages;
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
 }
 
 async function createMessage(data) {
-  try {
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    const myDB = client.db(dbName);
-    const myColl = myDB.collection(messageCollection);
-    // const doc = { name: "Neapolitan pizza", shape: "round" };
-    const result = await myColl.insertOne(data);
-    console.log(`A document was inserted with the _id: ${result.insertedId}`);
-    console.log("result object", result);
-    return result;
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        // Connect the client to the server (optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        const myDB = client.db(dbName);
+        const myColl = myDB.collection(messageCollection);
+        // const doc = { name: "Neapolitan pizza", shape: "round" };
+        const result = await myColl.insertOne(data);
+        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+        console.log("result object", result);
+        return result;
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
 }
--- expressServer.js
+++ expressServer.js
@@ -1,6 +1,7 @@
 const express = require("express");
 var cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const messagesApi = require(".messegesApi.js");
 
 const hostname = "127.0.0.1";
 const port = 3003;
@@ -9,27 +10,28 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use("/messages", messagesApi);
 
 //Set up connection to MongoDB
 const uri = "mongodb://localhost:27017";
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
 });
 
 app.post("/destinations", (req, res) => {
-  // send back the created object or at least the new ID
-  console.log(req.body);
-  createDestination(req.body);
-  res.send("Got a POST request");
+    // send back the created object or at least the new ID
+    console.log(req.body);
+    createDestination(req.body);
+    res.send("Got a POST request");
 });
 
 app.get("/destinations", (req, res) => {
-  getAllDestinations();
-  res.send("Getting travel destinations");
+    getAllDestinations();
+    res.send("Getting travel destinations");
 });
 
 // app.get("/destinations/:id", (req, res) => {
@@ -38,113 +40,113 @@ app.get("/destinations", (req, res) => {
 // });
 
 app.put("/destinations/:id", (req, res) => {
-  console.log("Updating destination with this id", req.params.id);
-  console.log("New data", req.body);
-  updateDestination(req.params.id, req.body);
-  res.send(`Got a PUT request to update destination with id ${req.params.id}`);
+    console.log("Updating destination with this id", req.params.id);
+    console.log("New data", req.body);
+    updateDestination(req.params.id, req.body);
+    res.send(`Got a PUT request to update destination with id ${req.params.id}`);
 });
 
 app.delete("/destinations/:id", (req, res) => {
-  console.log("delete destination with this id", req.params.id);
-  res.send("Got a DELETE request at /destinations");
-  deleteDestination(req.params.id);
+    console.log("delete destination with this id", req.params.id);
+    res.send("Got a DELETE request at /destinations");
+    deleteDestination(req.params.id);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
 
 async function createDestination(newDestination) {
-  try {
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    const myDB = client.db("travel");
-    const myColl = myDB.collection("destinations");
+    try {
+        // Connect the client to the server (optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        const myDB = client.db("travel");
+        const myColl = myDB.collection("destinations");
 
-    const result = await myColl.insertOne(newDestination);
-    console.log(`A document was inserted with the _id: ${result.insertedId}`);
-    console.log("result object", result);
-    return result;
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+        const result = await myColl.insertOne(newDestination);
+        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+        console.log("result object", result);
+        return result;
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
 }
 
 async function getAllDestinations() {
-  try {
-    // Connect the client to the server
-    await client.connect();
-    const myDB = client.db("travel");
-    const myColl = myDB.collection("destinations");
+    try {
+        // Connect the client to the server
+        await client.connect();
+        const myDB = client.db("travel");
+        const myColl = myDB.collection("destinations");
 
-    const destinations = await myColl.find({}).toArray();
-    console.log(destinations);
-    return destinations;
-  } finally {
-    await client.close();
-  }
+        const destinations = await myColl.find({}).toArray();
+        console.log(destinations);
+        return destinations;
+    } finally {
+        await client.close();
+    }
 }
 
 async function deleteDestination(destinationId) {
-  try {
-    // Connect the client to the server
-    await client.connect();
-    const myDB = client.db("travel");
-    const myColl = myDB.collection("destinations");
+    try {
+        // Connect the client to the server
+        await client.connect();
+        const myDB = client.db("travel");
+        const myColl = myDB.collection("destinations");
 
-    // Safely create an OBjectId from the string Id
-    const query = {
-      _id: ObjectId.isValid(destinationId) ? new ObjectId(destinationId) : null,
-    };
+        // Safely create an OBjectId from the string Id
+        const query = {
+            _id: ObjectId.isValid(destinationId) ? new ObjectId(destinationId) : null,
+        };
 
-    if (!query._id) {
-      console.log("Invalid format.");
-      return; // Exit early if the Id is not valid
+        if (!query._id) {
+            console.log("Invalid format.");
+            return; // Exit early if the Id is not valid
+        }
+        const result = await myColl.deleteOne(query);
+        if (result.deletedCount === 1) {
+            console.log(`Destination with id ${destinationId} deleted`, query);
+        } else {
+            console.log(`Destination with id ${destinationId} not found`);
+        }
+    } finally {
+        await client.close();
     }
-    const result = await myColl.deleteOne(query);
-    if (result.deletedCount === 1) {
-      console.log(`Destination with id ${destinationId} deleted`, query);
-    } else {
-      console.log(`Destination with id ${destinationId} not found`);
-    }
-  } finally {
-    await client.close();
-  }
 }
 
 async function updateDestination(destinationId, newData) {
-  try {
-    // Connect the client to the server
-    await client.connect();
-    const myDB = client.db("travel");
-    const myColl = myDB.collection("destinations");
+    try {
+        // Connect the client to the server
+        await client.connect();
+        const myDB = client.db("travel");
+        const myColl = myDB.collection("destinations");
 
-    // Safely create an ObjectId from the string Id
-    const query = {
-      _id: ObjectId.isValid(destinationId) ? new ObjectId(destinationId) : null,
-    };
+        // Safely create an ObjectId from the string Id
+        const query = {
+            _id: ObjectId.isValid(destinationId) ? new ObjectId(destinationId) : null,
+        };
 
-    if (!query._id) {
-      console.log("Invalid format.");
-      return; // Exit early if the Id is not valid
+        if (!query._id) {
+            console.log("Invalid format.");
+            return; // Exit early if the Id is not valid
+        }
+
+        // Create the update operation
+        const update = {
+            $set: newData,
+        };
+
+        // Update the document
+        const result = await myColl.updateOne(query, update);
+
+        if (result.matchedCount === 1) {
+            console.log(`Destination with id ${destinationId} updated`, update);
+        } else {
+            console.log(`Destination with id ${destinationId} not found`);
+        }
+    } finally {
+        await client.close();
     }
-
-    // Create the update operation
-    const update = {
-      $set: newData,
-    };
-
-    // Update the document
-    const result = await myColl.updateOne(query, update);
-
-    if (result.matchedCount === 1) {
-      console.log(`Destination with id ${destinationId} updated`, update);
-    } else {
-      console.log(`Destination with id ${destinationId} not found`);
-    }
-  } finally {
-    await client.close();
-  }
 }
